da472a4e9e5be71826da56768f7ffa61e62a4ff1
Rory Liberty <roryliberty@gmail.com>
2020-12-15 21:56:50 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.read = read;
+}
+
+const testBookOne = new Book('Book One', 'Rainne Liberty', 42, true);
+
+console.log("test");
+
+function addBookToLibrary() {
+	// do something...
+}
+
+//Write a function that loops through the array and displays each book on the page.
+
