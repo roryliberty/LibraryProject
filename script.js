bef7f2a077399824d97ca94df8c06194455bb9e7
Rory Liberty <roryliberty@gmail.com>
2020-12-17 22:47:26 +0000

    Remove button is now working!

--- script.js
+++ script.js
@@ -1,109 +1,87 @@
 let myLibrary = [];
 const bookshelfDiv = document.querySelector('#bookshelf');
-const submitButton = document.querySelector('#submit-btn');
+const submitButton = document.querySelector('#submit-btn').addEventListener('click', bookEntry);
 const removeButtons = document.querySelectorAll('.remove-btn');
 const modal = document.querySelector('#modal');
-const btn = document.querySelector('#add-btn');
-const span = document.querySelectorAll('.close')[0];
+const addButton = document.querySelector('#add-btn').addEventListener('click', () => modal.style.display = 'block');
+const span = document.querySelectorAll('.close')[0].addEventListener('click', () => modal.style.display = 'none');
 
 // Constructor
 function Book(title, author, pages, read) {
-	this.title = title;
-	this.author = author;
-	this.pages = pages;
-	this.read = read;
+	this.title = form.title.value;
+	this.author = form.author.value;
+	this.pages = form.pages.value + ' pages';
+	this.read = form.read.checked;
 }
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = 'block';
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = 'none';
-}
-
-// get info from book entry form and add to myLibrary
+// Get info from modal form and add to myLibrary
 function bookEntry() {
-	modal.style.display = 'none';
+	event.preventDefault();
 
-	let addTitle = document.querySelector('#input-title').value;
-	let addAuthor = document.querySelector('#input-author').value;
-	let addPages = document.querySelector('#input-pages').value;
-	let addRead = null;
-
-	if (document.querySelector('#input-read').checked == true) {
-		addRead = true;
+	if (form.title.value === '' || form.author.value === '' || form.pages.value === '') {
+		alert("Please fill out all information before clicking on Submit!")
 	} else {
-		addRead = false;
-	}
+		modal.style.display = 'none';
 
-	const newBook = new Book(addTitle, addAuthor, addPages, addRead);
-	myLibrary.push(newBook);
-	console.log(myLibrary);
-	showBooks();
-}
-
-// show the books on the page
-function showBooks() {
-	for (let i = (myLibrary.length - 1); i < myLibrary.length; i++) {
-
-		let protoDiv = document.createElement('div');
-		protoDiv.classList = 'cardDiv';
-		bookshelfDiv.appendChild(protoDiv);
-
-		let protoTitle = document.createElement('h1');
-		protoTitle.classList = 'cardTitle';
-		protoTitle.textContent = myLibrary[i].title;
-		protoDiv.appendChild(protoTitle);
-
-		let protoAuthor = document.createElement('p');
-		protoAuthor.classList = 'cardP';
-		protoAuthor.textContent = myLibrary[i].author;
-		protoDiv.appendChild(protoAuthor);
-
-		let protoPages = document.createElement('p');
-		protoPages.classList = 'cardP';
-		protoPages.textContent = myLibrary[i].pages + ' pages';
-		protoDiv.appendChild(protoPages);
-
-		let protoRead = document.createElement('p');
-		protoRead.classList = 'cardP';
-		protoRead.textContent = readOrNot(myLibrary[i].read);
-		protoDiv.appendChild(protoRead);
-
-		let protoButton = document.createElement('button');
-		protoButton.classList = 'remove-btn';
-		protoButton.textContent = 'Remove Book';
-		protoDiv.appendChild(protoButton);
-
-//		protoButton.addEventListener('click', () => {
-//			myLibrary.splice(myLibrary)
-//		});
+		const newBook = new Book(title, author, pages, read);
+		myLibrary.push(newBook);
+		form.reset();
+		resetBooks();
 	}
 }
 
+// Works with the Remove Book button
+function resetBooks() {
+	const display = document.querySelector('#bookshelf');
+	const cards = document.querySelectorAll('.cardDiv');
+	
+	for (let i = 0; i < cards.length; i++) {
+		display.removeChild(cards[i]);
+	}
 
+	for (let j = 0; j < myLibrary.length; j++) {
+		showBooks(myLibrary[j]);
+	}
+}
+
+// Display the books on the page
+function showBooks(item) {
+	let protoDiv = document.createElement('div');
+	protoDiv.classList = 'cardDiv';
+	protoDiv.setAttribute('id', myLibrary.indexOf(item));
+	bookshelfDiv.appendChild(protoDiv);
+
+	let protoTitle = document.createElement('h1');
+	protoTitle.classList.add = 'cardTitle';
+	protoTitle.textContent = item.title;
+	protoDiv.appendChild(protoTitle);
+
+	let protoAuthor = document.createElement('p');
+	protoAuthor.classList = 'cardP';
+	protoAuthor.textContent = item.author;
+	protoDiv.appendChild(protoAuthor);
+
+	let protoPages = document.createElement('p');
+	protoPages.classList = 'cardP';
+	protoPages.textContent = item.pages;
+	protoDiv.appendChild(protoPages);
+
+	let protoRead = document.createElement('p');
+	protoRead.classList = 'cardP';
+	protoRead.textContent = readOrNot(item.read);
+	protoDiv.appendChild(protoRead);
+
+	let protoButton = document.createElement('button');
+	protoButton.classList = 'remove-btn';
+	protoButton.textContent = 'Remove Book';
+	protoDiv.appendChild(protoButton);
+
+	protoButton.addEventListener('click', () => {
+		myLibrary.splice(myLibrary.indexOf(item), 1);
+		resetBooks();
+	});
+}
 
 function readOrNot(bool) {
 	return (bool === true) ? 'Read' : 'Not Read';
-}
-
-
-
-function removeBook() {
-	
-}
-
-
-
-submitButton.addEventListener('click', bookEntry);
-
-
-
-for (const removeButton of removeButtons) {
-	removeButton.addEventListener('click', function(e) {
-		console.log(e.target);
-	});
 }
